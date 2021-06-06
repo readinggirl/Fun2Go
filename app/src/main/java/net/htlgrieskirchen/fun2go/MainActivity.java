8b4371aafb12ce71448f566d3a962c450071acca
Teresa Edinger <Edinger.Teresa@sus.htl-grieskirchen.at>
2021-06-06 14:30:15 +0200

    main screen optimised

--- app/src/main/java/net/htlgrieskirchen/fun2go/MainActivity.java
+++ app/src/main/java/net/htlgrieskirchen/fun2go/MainActivity.java
@@ -3,9 +3,22 @@ package net.htlgrieskirchen.fun2go;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
