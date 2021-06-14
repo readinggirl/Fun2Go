6a7bf6da877d6bd5e54b11ef43c5d80a27bfebb2
LisaHolzmann <holzmann.lisa@gmx.at>
2021-06-14 11:00:23 +0200

    Class MainMenu onClickListener added

--- app/src/main/java/net/htlgrieskirchen/fun2go/MainMenu.java
+++ app/src/main/java/net/htlgrieskirchen/fun2go/MainMenu.java
@@ -1,6 +1,8 @@
 package net.htlgrieskirchen.fun2go;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,24 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button themen = findViewById(R.id.themen);
+        Button zufall = findViewById(R.id.zufall);
+
+        themen.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ThemenAuswahl themenAuswahl = new ThemenAuswahl();
+            }
+        });
+
+        zufall.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ShowFact showFact = new ShowFact("zufall");
+            }
+        });
     }
 }
